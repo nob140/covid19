2fee5ec1537f144fc90544f25f06ef47dd0fcf3c
Nobu <7860590+nob140@users.noreply.github.com>
2020-05-06 21:58:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,8 @@ async function init() {
 	var filename = "covid19_" + todaystr + ".csv";
 	
 	//2020-05-06 Using local file tentatively
-	filename = "covid19_2020-05-06.csv";
+	//filename = "covid19_2020-05-06.csv";
+	
 	var filepath = "./data/" + filename;
 
 	//Read data
