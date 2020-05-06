b151c675591117ba9bc63e1839ed2af51d1b4241
Nobu <7860590+nob140@users.noreply.github.com>
2020-05-06 22:05:59 +0900

    Update initData.js

--- initData.js
+++ initData.js
@@ -27,6 +27,7 @@ function getFile(filepath){
 		req.open("GET", filepath, true);
 		req.onload = () => {
 			if (req.readyState === 4 && req.status === 200) {
+				alert("success req.onload");
 				resolve(req.responseText);
 			}else{
 				alert(req.statusText);
