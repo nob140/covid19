0eaf1d464ab6c0d76eda8f83e78091c24c8d0e2b
nob140 <nob140@users.noreply.github.com>
2020-05-06 22:11:13 +0900

    Update initData.js

--- initData.js
+++ initData.js
@@ -30,12 +30,12 @@ function getFile(filepath){
 				alert("success req.onload");
 				resolve(req.responseText);
 			}else{
-				alert("error 1");
+				alert("error 1: " + filepath);
 				reject(new Error(req.statusText));
 			}
 		};
 		req.onerror = () => {
-			alert("error 2");
+			alert("error 2: " + filepath);
 			reject(new Error(req.statusText));
 		};
 		req.send(null);
